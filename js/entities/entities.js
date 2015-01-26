b683c42501827209a6047b5ec607fbc75c8b6556
Luca Veca <lucaveca@gmail.com>
2015-01-26 11:56:43 -0800

    Video 10(Last push was supposed to be 8 and 9)

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -18,6 +18,11 @@ game.PlayerEntity = me.Entity.extend({
 		//the speed the player moves at
 		this.body.setVelocity(5, 20);
 
+		this.renderable.addAnimation("idle", [78])
+		this.renderable.addAnimation("walk", [177, 118, 119, 120, 121, 122, 123, 124, 125], 80)
+
+		this.renderable.setCurrentAnimation("idle");
+
 	},
 
 	//when the right key is pressed the character will move to the right
@@ -27,12 +32,25 @@ game.PlayerEntity = me.Entity.extend({
 			//setVelocity() and multiplying it by me.timer.tick.
 			//me.timer.tick makes the movement look smooth
 			this.body.vel.x += this.body.accel.x * me.timer.tick;
+			this.flipX(true);
 		}
 		else{
 			this.body.vel.x = 0;
 		}
 
+		if(this.body.vel.x !== 0){
+			if(!this.renderable.isCurrentAnimation("walk")){
+				this.renderable.setCurrentAnimation("walk");
+			}
+		}
+		else{
+			this.renderable.setCurrentAnimation("idle");
+		}
+
+
 		this.body.update(delta);
+
+		this._super(me.Entity, "update", [delta]);
 		return true;
 	}
 
