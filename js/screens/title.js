c85b8352093e2fc528aa144a8a7fca18680ac5cd
Luca Veca <lucaveca@gmail.com>
2015-02-24 10:44:10 -0800

    Video 27 comments

--- js/screens/title.js
+++ js/screens/title.js
@@ -6,15 +6,19 @@ game.TitleScreen = me.ScreenObject.extend({
 		//calls the image you set as the title screen
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); // TODO
 
+		//makes the enter key the trigger that starts the game
 		me.input.bindKey(me.input.KEY.ENTER, "start");
 
 		me.game.world.addChild(new (me.Renderable.extend({
 			init: function(){
+				//renders the text
 				this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height]);
+				//font and font color
 				this.font = new me.Font("Arial", 46, "white");
 			},
 
 			draw: function(renderer){
+				//sets the position on the screen for the text
 				this.font.draw(renderer.getContext(), "Awesomenauts!", 450, 130);
 				this.font.draw(renderer.getContext(), "Press ENTER to play", 250, 530);
 
