2f794ffaffaa5ca5163f99b29e1631242b66fb3e
Luca Veca <lucaveca@gmail.com>
2015-03-06 19:01:59 -0800

    Video 38 with comments

--- js/screens/title.js
+++ js/screens/title.js
@@ -6,30 +6,21 @@ game.TitleScreen = me.ScreenObject.extend({
 		//calls the image you set as the title screen
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); // TODO
 
-		//makes the enter key the trigger that starts the game
-		me.input.bindKey(me.input.KEY.ENTER, "start");
-
 		me.game.world.addChild(new (me.Renderable.extend({
 			init: function(){
 				//renders the text
-				this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height]);
+				this._super(me.Renderable, 'init', [270, 240, 300, 50]);
 				//font and font color
 				this.font = new me.Font("Arial", 46, "white");
 			},
 
 			draw: function(renderer){
 				//sets the position on the screen for the text
-				this.font.draw(renderer.getContext(), "Awesomenauts!", 450, 130);
-				this.font.draw(renderer.getContext(), "Press ENTER to play", 250, 530);
+				this.font.draw(renderer.getContext(), "START A NEW GAME", 450, 130);
+				this.font.draw(renderer.getContext(), "Press ENTER to play", this.pos.x, this.pos.y);
 
 			}
 		})));
-
-		this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge){
-			if(action === "start"){
-				me.state.change(me.state.PLAY);
-			}
-		});
 	},
 	
 	
@@ -37,7 +28,6 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-		me.input.unbindKey(me.input.KEY.ENTER); // TODO
-		me.event.unsubscribe(this.handler);
+
 	}
 });
