0182a5f8e0d55841662796f7cd539c43a53f3fd7
Luca Veca <lucaveca@gmail.com>
2015-03-10 17:22:31 -0700

    Video 42 with comments

---
+++ js/screens/spendExp.js
@@ -0,0 +1,32 @@
+game.SpendExp = me.ScreenObject.extend({
+	/**	
+	 *  action to perform on state change
+	 */
+	onResetEvent: function() {	
+		//calls the image you set as the title screen
+		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('exp-screen')), -10); // TODO
+
+		me.game.world.addChild(new (me.Renderable.extend({
+			init: function(){
+				//renders the text
+				this._super(me.Renderable, 'init', [270, 240, 300, 50]);
+				//font and font color
+				this.font = new me.Font("Arial", 46, "white");
+			},
+
+			draw: function(renderer){
+				//sets the position on the screen for the text
+				this.font.draw(renderer.getContext(), "SPEND", this.pos.x, this.pos.y);
+
+			},
+		})));
+
+	},
+	
+	/**	
+	 *  action to perform when leaving this screen (state change)
+	 */
+	onDestroyEvent: function() {
+
+	}
+});
