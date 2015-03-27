17efa5800e1b78aaa4bf9004055a4c42e70df240
Luca Veca <lucaveca@gmail.com>
2015-03-27 10:16:25 -0700

    Video 50 and 51 with comments

---
+++ js/entities/gamemanagers/HeroDeathManager.js
@@ -0,0 +1,18 @@
+game.HeroDeathManager = Object.extend({
+	init: function(x, y, settings){
+		this.alwaysUpdate = true;
+	},
+
+	update: function(){
+		//runs if player is dead
+		if(game.data.player.dead){
+			//takes the player off the screen
+			me.game.world.removeChild(game.data.player);
+			//runs the resetPlayer function
+			me.state.current().resetPlayer(10, 0);
+
+		}
+
+		return true;
+	}
+});
