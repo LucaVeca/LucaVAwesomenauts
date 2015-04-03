7cd4030f9984dced20a7e59a22a239128bbbdce1
Luca Veca <lucaveca@gmail.com>
2015-04-02 17:23:18 -0700

    Video 55

--- data/php/controller/create-db.php
+++ data/php/controller/create-db.php
@@ -1,27 +1,6 @@
 <?php
 	//links to database.php
 	require_once(__DIR__ . "/../model/config.php");
-	
-	
-
-	//Table within the database
-	$query = $_SESSION["connection"]->query("CREATE TABLE posts ("
-		//columns in table
-		//id
-		. "id int(11) NOT NULL AUTO_INCREMENT,"
-		//title		
-		. "title varchar(255) NOT NULL,"
-		//post
-		. "post text NOT NULL,"
-		//way tables are connected to each other
-		. "PRIMARY KEY(id))");
-
-	if($query){
-			
-	}
-	else {
-		// echo "<p>" . $_SESSION["connection"]->error ."</p>";
-	}
 
 	$query = $_SESSION["connection"]->query("CREATE TABLE users ("
 		//columns in table
@@ -32,6 +11,11 @@
 		. "email varchar(50) NOT NULL,"
 		. "password varchar(128) NOT NULL,"
 		. "salt char(128) NOT NULL,"
+		. "exp int(4),"
+		. "exp1 int(4),"
+		. "exp2 int(4),"
+		. "exp3 int(4),"
+		. "exp4 int(4),"
 		. "DateTime datetime NOT NULL,"
 		//way tables are connected to each other
 		. "PRIMARY KEY(id))");
